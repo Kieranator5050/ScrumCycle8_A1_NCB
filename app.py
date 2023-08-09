df428c981c821b1992cb7f5420bd58abd7bd08c2
Kieran Jaggernauth <thejugg63@gmail.com>
2023-08-08 20:20:03 -0500

    Added templates

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
+
+"""MOCK DATA"""
+
+""" 
+Context processor
+- Injects variable into every template
+"""
+@app.context_processor
+def inject_date():
+    return dict(date=datetime.now())
+
 @app.route("/")
 def home():
-    return "<p>Home</p>"
+    return render_template('home.html')
 
 
 @app.route("/products")
 def product():
-    return "<p>Products</p>"
+    return render_template('products.html')
 
 
 @app.route("/products/<product_id>")
 def product_detail(product_id):
-    return f'<p>Product {product_id} Detail</p>'
+    return render_template('product_detail.html')
